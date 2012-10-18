9ffb165acbfd4b3ed10c1a56f16d1556f1ec5e67
Andrew Wagner <wagner.andrew@gmail.com>
2012-10-18 17:57:04 -0400

    episode 2

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,9 +1,23 @@
 package com.thecherno.rain;
 
-public class Game {
+public class Game implements Runnable {
 
-		public static int width = 300;
-		public static int height = width / 16 * 9;
-		public static int scale = 3;
-		
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+
+	private Thread thread;
+
+	public synchronized void start() {
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+
+		}
+	}
 }
