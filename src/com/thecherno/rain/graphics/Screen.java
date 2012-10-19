d3a837bfcf40d27e447b11235b90c557c10fe00a
Andrew Wagner <wagner.andrew@gmail.com>
2012-10-18 20:29:24 -0400

    episode 10

--- src/com/thecherno/rain/graphics/Screen.java
+++ src/com/thecherno/rain/graphics/Screen.java
@@ -5,16 +5,29 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 	
+	int time = 0;
+	int counter = 0;
+	
 	public Screen(int width, int height){
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
 	}
 	
+	public void clear(){
+		for (int i = 0; i < pixels.length; i++){
+			pixels[i] = 0;
+		}
+	}
+	
 	public void render(){
+		counter++;
+		if (counter % 100 == 0){
+			time++;
+		}
 		for (int y = 0; y < height; y++){
 			for (int x = 0; x < width; x++){
-				pixels[40 + 30 * width] = 0xff00ff;
+				pixels[time + time * width] = 0xff00ff;
 			}
 		}
 	}
