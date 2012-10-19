10cda8e65495a07f4e5da9b5163be7321920daae
Andrew Wagner <wagner.andrew@gmail.com>
2012-10-18 20:02:37 -0400

    episode 8

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.thecherno.rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
@@ -21,6 +23,8 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	private boolean running = false;
 	
+	private Screen screen;
+	
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
@@ -28,6 +32,7 @@ public class Game extends Canvas implements Runnable {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);		
 		
+		screen = new Screen(width, height);
 		frame = new JFrame();
 	}
 	
---
+++ src/com/thecherno/rain/graphics/Screen.java
@@ -0,0 +1,13 @@
+package com.thecherno.rain.graphics;
+
+public class Screen {
+	
+	private int width, height;
+	public int[] pixels;
+	
+	public Screen(int width, int height){
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+	}
+}
