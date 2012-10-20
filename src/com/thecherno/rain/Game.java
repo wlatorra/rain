362e42cb2d99b37cb82f795aeb972b37e8c7f1c8
Andrew Wagner <wagner.andrew@gmail.com>
2012-10-19 20:31:36 -0400

    episode 14

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -18,7 +18,8 @@ public class Game extends Canvas implements Runnable {
 	public static int width = 300;
 	public static int height = 168;
 	public static int scale = 3;
-
+	public static String title = "Rain";
+	
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
@@ -52,10 +53,32 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void run(){
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 		while (running){
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1){
+				update();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println(updates + " ups, " + frames + " fps");
+				frame.setTitle(title + " | " + updates + " ups, " + frames + " fps");
+				frames = updates = 0;
+			}
 		}
+		stop();
 	}
 	
 	public void update(){
@@ -84,7 +107,7 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args){
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("Rain");
+		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/com/thecherno/rain/graphics/Screen.java
+++ src/com/thecherno/rain/graphics/Screen.java
@@ -26,9 +26,9 @@ public class Screen {
 		if (counter % 100 == 0) ytime--;
 
 		for (int y = 0; y < height; y++){
-			if (ytime >= height) break;
+			if (ytime < 0 || ytime >= height) break;
 			for (int x = 0; x < width; x++){
-				if (xtime >= width) break;
+				if (xtime < 0 || xtime >= width) break;
 				pixels[xtime + ytime * width] = 0xff00ff;
 			}
 		}
